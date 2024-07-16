7dfeb4827721fffa26ee6c2e020f3e04f4af8283
Vilma Sand <vilma.sandonaviciene@gmail.com>
2024-07-16 13:20:14 +0300

    Create test whenVisitorEntersOneSymbolLastName_theReturn400AndResponseBody

--- API_tests/src/test/java/RegistrationTests.java
+++ API_tests/src/test/java/RegistrationTests.java
@@ -415,6 +415,40 @@ public class RegistrationTests {
     }
 
     @Test
+    void whenVisitorEntersOneSymbolLastName_theReturn400AndResponseBody() {
+
+        given().body(
+                        """
+                {
+                    "firstName": "Testas",
+                    "lastName": "T",
+                    "country": "Lithuania",
+                    "password": "Testas123*",
+                    "displayName": "Jukava",
+                    "roles": [
+                        {
+                            "id": 1
+                        }
+                    ],
+                    "dateOfBirth": "1903-01-01",
+                    "email": "jukava@testas.lt"
+                }
+                """)
+                .contentType(ContentType.JSON)
+                .when()
+                .request("POST", "/register")
+                .then()
+                .assertThat()
+                .statusCode(400)
+                .body(
+                        "size()",
+                        is(1),
+                        "lastName",
+                        equalTo(
+                                "You can only enter letters. First letter must be capital. At least 2 characters long"));
+    }
+
+    @Test
     void whenVisitorEntersLastName_theReturn400AndResponseBody() {
 
         given().body(
